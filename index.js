3e9f696a7525efbf042cf57ba0b23779e5e70c06
Zcy. <157526994+4ozCy@users.noreply.github.com>
2024-10-24 13:40:41 +0700

    Update and rename Bot.js to index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ client.on('interactionCreate', async (interaction) => {
             const formData = new FormData();
             formData.append('file', response.data, file.name);
 
-            const uploadResponse = await axios.post('http://files-box.vercel.app/api/file', formData, {
+            const uploadResponse = await axios.post('https://filesbox.glitch.me/api/file', formData, {
                 headers: formData.getHeaders(),
             });
 
